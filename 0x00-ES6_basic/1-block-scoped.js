6797e4f1bbf322538f80a7c2170e1e4d7bf1a426
Wise-deldu <delduwise@gmail.com>
2024-08-07 01:45:06 +0000

    Deleted empty spaces and change const to let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  let task = false;
+  let task2 = true;
 
-    if (trueOrFalse) {
-    }
+  if (trueOrFalse) {
+    task2 = true;
+    task = false;
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
