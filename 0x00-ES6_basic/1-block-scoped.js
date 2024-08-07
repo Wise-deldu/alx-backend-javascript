663145dad65ef02e1bf27d0602250f71b869300e
Wise-deldu <delduwise@gmail.com>
2024-08-07 01:53:10 +0000

    Removed the let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task2 = true;
-    task = false;
   }
 
   return [task, task2];
