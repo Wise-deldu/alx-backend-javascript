08def087d0b10af3c8a6e333c70d925c08aa0a95
Wise-deldu <delduwise@gmail.com>
2024-08-08 20:05:03 +0000

    Corrected typos and empty spaces

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,12 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const res = [];
   try {
     const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfiled', value: user });
+    res.push({ status: 'fulfilled', value: user });
     await uploadPhoto(fileName);
   } catch (err) {
     res.push({
-        status: 'rejected',
-        value: `Error: ${fileName} cannot be processed`,
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
     });
   }
   return res;
