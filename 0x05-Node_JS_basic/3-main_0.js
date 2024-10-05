cd3a5ef5f8753f37eb38aab16f70225af3700f62
Wise-deldu <delduwise@gmail.com>
2024-10-05 06:43:40 +0000

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-main_0.js
@@ -0,0 +1,9 @@
+import countStudents from './3-read_file_async';
+
+countStudents('nope.csv')
+  .then(() => {
+    console.log('Done!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
