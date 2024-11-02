b3c41017adbb4fc2b86347d6b8f3d0644888d546
Wise-deldu <delduwise@gmail.com>
2024-11-02 20:51:36 +0000

    Code refactor

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,30 +1,31 @@
 const express = require('express');
+
 const app = express();
 const PORT = 7865;
 
-app.use(express.json()); 
+app.use(express.json());
 
-// Root endpoint
 app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
-// GET /cart/:id route with numeric-only path validation
 app.get('/cart/:id(\\d+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`); // Responds with a message including the cart ID
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-// GET /available_payments endpoint to return available payment methods
 app.get('/available_payments', (_req, res) => {
   res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-// POST /login endpoint
 app.post('/login', (req, res) => {
-  const username = req.body?.userName || '';
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
   res.send(`Welcome ${username}`);
 });
-
 
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
