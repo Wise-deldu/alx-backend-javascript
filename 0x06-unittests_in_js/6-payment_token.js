38f9cb87b440cd4686a164f566ac9c5e26e737c3
Wise-deldu <delduwise@gmail.com>
2024-11-02 18:41:26 +0000

    Task 6 - code refactor

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,14 +1,7 @@
-function getPaymentTokenFromAPI(success) {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ data: 'Successful response from the API' });
-      } else {
-        // We are not doing anything if success is false, 
-        // but we can reject the promise if desired
-        // reject('Error: API call failed');
-      }
-    });
-}
-  
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API' });
+  }
+});
+
 module.exports = getPaymentTokenFromAPI;
